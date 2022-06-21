b153fc2fc206ba68eb456d032e83f85c69fb3d8c
jchyczynski <jasiek.ch16@gmail.com>
2022-06-21 19:43:25 +0200

    moved players to array in MainLoop

--- src/main/scala/Logic/PlayerBoat.scala
+++ src/main/scala/Logic/PlayerBoat.scala
@@ -1,7 +1,15 @@
 package Logic
 
-class PlayerBoat (private var speed: Double, private var heading: Double, var position: Vector2d,
+import Gui.KeyPolling
+import scalafx.scene.input.KeyCode
+
+class PlayerBoat (private val keyPolling: KeyPolling, private var speed: Double, private var heading: Double, var position: Vector2d,
                   private var steerDeflection: Double = 0.0, private var lastTimeEval: Int = 0){
+  def handleInput(timeElapsed: Long): Unit = {
+    if (keyPolling.isDown(KeyCode.Down)) {
+      position += Vector2d(-timeElapsed / 1E8, timeElapsed / 1E7)
+    }
+  }
 
   def calcPosition(nanoTimeElapsed: Long): Unit ={
 

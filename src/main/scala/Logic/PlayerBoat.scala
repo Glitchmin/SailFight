ac9399dbdbed52a35113d87a6958a301efe07704
Adam <adam.p.kulczycki@gmail.com>
2022-06-21 20:26:28 +0200

    implemented steer changes and methods

--- src/main/scala/Logic/PlayerBoat.scala
+++ src/main/scala/Logic/PlayerBoat.scala
@@ -1,11 +1,40 @@
+
 package Logic
 
-class PlayerBoat (private var speed: Double, private var heading: Double, var position: Vector2d,
-                  private var steerDeflection: Double = 0.0, private var lastTimeEval: Int = 0){
+import scala.math.{cos, max, min, sin, toRadians}
 
-  def calcPosition(nanoTimeElapsed: Long): Unit ={
 
-    position += Vector2d(nanoTimeElapsed/1E7, nanoTimeElapsed/1E8)
-    println(position)
+object PlayerBoat {
+  val steerDeflectionSpeed = 10.0; //degrees per second
+  val maxDeflection = 60.0; //degrees
+}
+
+class PlayerBoat(private var speed: Double, private var heading: Double, var position: Vector2d,
+                 private var steerDeflection: Double = 0.0, private var lastTimeEval: Int = 0) {
+
+  private def nanoSecondIntoSecond(nanoTime: Long): Double = {
+    nanoTime / 1e9
   }
+
+  def calcPosition(nanoTimeElapsed: Long): Unit = {
+    val rad = toRadians(heading)
+    position += Vector2d((nanoTimeElapsed * sin(rad)) / 1E8, -(nanoTimeElapsed * cos(rad)) / 1E8)
+    heading += steerDeflection * nanoSecondIntoSecond(nanoTimeElapsed)
+  }
+
+
+  def deflectSteerRight(nanoTimeElapsed: Long): Unit = {
+    steerDeflection += PlayerBoat.steerDeflectionSpeed * nanoSecondIntoSecond(nanoTimeElapsed)
+    steerDeflection = min(PlayerBoat.maxDeflection, steerDeflection)
+  }
+
+  def deflectSteerLeft(nanoTimeElapsed: Long): Unit = {
+    steerDeflection -= PlayerBoat.steerDeflectionSpeed * nanoSecondIntoSecond(nanoTimeElapsed)
+    steerDeflection = max(-PlayerBoat.maxDeflection, steerDeflection)
+  }
+
+  def SteerDeflection: Double = {
+    steerDeflection
+  }
+
 }
